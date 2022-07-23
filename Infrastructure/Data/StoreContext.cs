547d18e5ee03244db5268fd8a0820247ec3023e9
DharmRaj15 <dharmaraj.raut@outlook.com>
2022-07-23 23:30:57 +0530

    Commit After Data Seeding

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Core.Entities;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
@@ -13,5 +14,11 @@ namespace Infrastructure.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
         public DbSet<ProductBrand> productBrands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
--- webApp/Startup.cs
+++ webApp/Startup.cs
@@ -14,7 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using webApp.Data;
+//using webApp.Data;
 
 namespace webApp
 {
